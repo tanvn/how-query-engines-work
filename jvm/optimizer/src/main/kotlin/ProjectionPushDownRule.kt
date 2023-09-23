920cc027759697b5902748def41f9481f4209296
tan.vu <tan.vu@linecorp.com>
2023-09-24 01:05:38 +0900

    Add comments

--- jvm/optimizer/src/main/kotlin/ProjectionPushDownRule.kt
+++ jvm/optimizer/src/main/kotlin/ProjectionPushDownRule.kt
@@ -14,40 +14,57 @@
 
 package io.andygrove.kquery.optimizer
 
-import java.lang.IllegalStateException
 import io.andygrove.kquery.logical.*
 
 class ProjectionPushDownRule : OptimizerRule {
 
-  override fun optimize(plan: LogicalPlan): LogicalPlan {
-    return pushDown(plan, mutableSetOf())
-  }
-
-  private fun pushDown(plan: LogicalPlan, columnNames: MutableSet<String>): LogicalPlan {
-
-    return when (plan) {
-      is Projection -> {
-        extractColumns(plan.expr, plan.input, columnNames)
-        val input = pushDown(plan.input, columnNames)
-        Projection(input, plan.expr)
-      }
-      is Selection -> {
-        extractColumns(plan.expr, plan.input, columnNames)
-        val input = pushDown(plan.input, columnNames)
-        Selection(input, plan.expr)
-      }
-      is Aggregate -> {
-        extractColumns(plan.groupExpr, plan.input, columnNames)
-        extractColumns(plan.aggregateExpr.map { it.expr }, plan.input, columnNames)
-        val input = pushDown(plan.input, columnNames)
-        Aggregate(input, plan.groupExpr, plan.aggregateExpr)
-      }
-      is Scan -> {
-        val validFieldNames = plan.dataSource.schema().fields.map { it.name }.toSet()
-        val pushDown = validFieldNames.filter { columnNames.contains(it) }.toSet().sorted()
-        Scan(plan.path, plan.dataSource, pushDown)
-      }
-      else -> throw IllegalStateException("ProjectionPushDownRule does not support plan: $plan")
+    override fun optimize(plan: LogicalPlan): LogicalPlan {
+        return pushDown(plan, mutableSetOf())
     }
-  }
+
+    private fun pushDown(plan: LogicalPlan, columnNames: MutableSet<String>): LogicalPlan {
+
+        return when (plan) {
+            // SELECT col1, col2 FROM tbl1
+            is Projection -> {
+                extractColumns(plan.expr, plan.input, columnNames)
+                val input = pushDown(plan.input, columnNames)
+                Projection(input, plan.expr)
+            }
+
+            // SELECT col3 FROM tbl WHERE col1 = 'A' AND col2 > 0
+            // plan.exp: WHERE col1 = 'A' AND col2 > 0 -> And(l: LogicalExpr, r: LogicalExpr)
+            // where:
+            //      l: Eq(l: LogicalExpr, r: LogicalExpr) -> l: col1 , r: 'A'
+            //      r: Lt(l: LogicalExpr, r: LogicalExpr): l: col2, r: 0
+            is Selection -> {
+                extractColumns(plan.expr, plan.input, columnNames) // col1, col2
+                // for example, the plan.input can be a Scan, then the returned input here will be a Scan
+                // (no additional recursive call when the plan is a Scan)
+                val input: LogicalPlan =
+                    pushDown(plan.input, columnNames) // recursive here as Selection has a child logical plan
+
+                Selection(input, plan.expr)
+            }
+
+            // SELECT MAX(col3) FROM tbl1 GROUP BY col1, col2
+            is Aggregate -> {
+                extractColumns(plan.groupExpr, plan.input, columnNames) // col1, col2
+                extractColumns(plan.aggregateExpr.map { it.expr }, plan.input, columnNames) // col3
+                val input =
+                    pushDown(plan.input, columnNames) // recursive here as Aggregate has a child logical plan (input)
+                Aggregate(input, plan.groupExpr, plan.aggregateExpr)
+            }
+
+            is Scan -> {
+                val validFieldNames =
+                    plan.dataSource.schema().fields.map { it.name }.toSet() // get all columns of the datasource
+                val pushDown: List<String> =
+                    validFieldNames.filter { columnNames.contains(it) }.toSet().sorted() // only get necessary columns
+                Scan(plan.path, plan.dataSource, pushDown)
+            }
+
+            else -> throw IllegalStateException("ProjectionPushDownRule does not support plan: $plan")
+        }
+    }
 }
